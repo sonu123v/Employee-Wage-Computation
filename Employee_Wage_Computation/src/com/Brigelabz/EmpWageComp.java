d6ca192f4491fa7da5ed0eb3775c69ca6ee54e9d
sonu parihar <sparihar729@gmail.com>
2021-08-21 14:27:26 +0530

    Add Part Time Employee And Wages

--- Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
+++ Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
@@ -5,18 +5,21 @@ public class EmpWageComp
 
 	public static void main(String[] args) 
 	{
-		System.out.println("welcome to the employee wages computation");
-		double a = Math.floor(Math.random()*10)%2;
-		int empHrs = 0;
+		System.out.println("Welcome To Employee Wage Computation");
+        double a = Math.floor(Math.random() * 10) % 3;
+        int empHrs = 0;
         if (a == 0) {
             System.out.println("Employee is Absent");
              empHrs = 0;
-        } else {
+        } else if(a==1) {
             System.out.println("Employee is Present");
              empHrs = 8;
+        }else if(a==2){
+            System.out.println("Employee doing Part Time");
+            empHrs = 3;
         }
         int emp_rate = 20;
         int empwage = empHrs * emp_rate;
-        System.out.println("Employee Wage is Rs."+empwage);
+System.out.println("Employee Wage is Rs."+empwage);
     }
 }
