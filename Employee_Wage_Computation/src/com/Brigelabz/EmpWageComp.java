b9f673dff01754516b3ceea27188cec45858aa51
sonu parihar <sparihar729@gmail.com>
2021-08-21 14:48:14 +0530

    Calculate Wages Till Condition Reached for a Month

--- Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
+++ Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
@@ -6,23 +6,22 @@ public class EmpWageComp
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome To Employee Wage Computation");
-        int a =(int) Math.floor(Math.random() * 10) % 3;
-        int empHrs = 0;
-        switch (a) {
-            case 0:
-                System.out.println("Employee is Absent");
-                empHrs = 0;
-                break;
-            case 1:
-                System.out.println("Employee is Present");
-                empHrs = 8;
-                break;
-            default:
-                System.out.println("Employee doing Part Time");
-                empHrs = 3;
+        int empHrs = 0, emp_rate = 20, totemphrs = 0, totworkday = 0;
+
+        while (totemphrs <= 100 && totworkday < 20) {
+            int a = (int) Math.floor(Math.random() * 10) % 3;
+            totworkday++;
+            switch (a) {
+                case 0:
+                    System.out.println("Employee is Absent");
+                    System.out.println("Employee doing Part Time");
+                    empHrs = 3;
+            }
+            totemphrs += empHrs;
+            System.out.println("Day " + totworkday + " Employee Hours: " + empHrs);
         }
-        int emp_rate = 20;
-        int empwage = empHrs * emp_rate;
-        System.out.println("Employee Wage is Rs." + empwage);
+        int totempwag = totemphrs * emp_rate;
+        System.out.println("Total Working Hours: " + totemphrs);
+        System.out.println("Total Employee wage for a Month Rs. " + totempwag);
     }
 }
