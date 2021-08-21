a5c1f9c56ff47ac2a50f216b1401f934dc81265f
sonu parihar <sparihar729@gmail.com>
2021-08-21 13:14:26 +0530

    Modified the spaces to neet and clean code

--- Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
+++ Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
@@ -1,13 +1,18 @@
 package com.Brigelabz;
 
-public class EmpWageComp {
+public class EmpWageComp 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("welcome to the employee wages computation");
 		double a = Math.floor(Math.random()*10)%2;
-        if (a==0){
+        if (a==0)
+         {
             System.out.println("Employee is Absent");
-        }else {
+         }
+        else 
+        {
             System.out.println("Employee is Present");
         }
 	}
