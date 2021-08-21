1566248ddf70759694fd8fbac416009dbd8d1f3b
sonu parihar <sparihar729@gmail.com>
2021-08-21 13:04:21 +0530

    Check Employee is Present or Not

--- Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
+++ Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
@@ -4,7 +4,12 @@ public class EmpWageComp {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to the employee wages computation");
-
+		double a = Math.floor(Math.random()*10)%2;
+        if (a==0){
+            System.out.println("Employee is Absent");
+        }else {
+            System.out.println("Employee is Present");
+        }
 	}
 
 }
