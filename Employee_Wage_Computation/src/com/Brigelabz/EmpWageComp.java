d4c29030ef38704c95caaf775d4de7d89e6d1fbe
sonu parihar <sparihar729@gmail.com>
2021-08-21 14:09:24 +0530

    Calculating the wages of employee

--- Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
+++ Employee_Wage_Computation/src/com/Brigelabz/EmpWageComp.java
@@ -7,14 +7,16 @@ public class EmpWageComp
 	{
 		System.out.println("welcome to the employee wages computation");
 		double a = Math.floor(Math.random()*10)%2;
-        if (a==0)
-         {
+		int empHrs = 0;
+        if (a == 0) {
             System.out.println("Employee is Absent");
-         }
-        else 
-        {
+             empHrs = 0;
+        } else {
             System.out.println("Employee is Present");
+             empHrs = 8;
         }
-	}
-
+        int emp_rate = 20;
+        int empwage = empHrs * emp_rate;
+        System.out.println("Employee Wage is Rs."+empwage);
+    }
 }
